8fe9d2041d63bb7f451f1ded0146da767b12665e
Franco Nicolas Holzman <franco.holzman@globant.com>
2018-10-31 15:04:43 -0300

    some minor changes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom';
 import { Hello } from './components/Hello';
 declare let module: any
 ReactDOM.render(<Hello compiler="Typescript" framework="React" bundler="Webpack" />,
-document.getElementById('root'));
+    document.getElementById('root'));
 if (module.hot) {
     module.hot.accept();
- }
+}
